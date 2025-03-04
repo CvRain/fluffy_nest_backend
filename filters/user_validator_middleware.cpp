e6c574381fb8e03d6aa060c4d60aa88d1d3ab957
cvrain <cvraindays@outlook.com>
2025-03-04 10:10:25 +0800

    重新添加对于用户验证的过滤器

--- filters/user_validator_middleware.cpp
+++ filters/user_validator_middleware.cpp
@@ -4,23 +4,133 @@
 
 #include "user_validator_middleware.hpp"
 
+#include "services/logger.hpp"
+#include "services/user_services.hpp"
+#include "types/nlohmann_json_request.hpp"
+#include "types/nlohmann_json_response.hpp"
+#include "types/schema.hpp"
+#include "types/type.hpp"
+#include "utils/exception_handler.hpp"
+
+
 namespace filter {
 
     void UserIdExistMiddleware::invoke(const HttpRequestPtr&    req,
                                        MiddlewareNextCallback&& nextCb,
-                                       MiddlewareCallback&&     mcb) {}
+                                       MiddlewareCallback&&     mcb) {
+        service::Logger::info("UserIdExistMiddleware::invoke");
+
+        try {
+            std::string user_id{};
+            if (req->method() != HttpMethod::Get) {
+                user_id = req->getJsonObject()->operator[](type::UserSchema::key_user_id.data()).as<std::string>();
+            }
+            else {
+                user_id = req->getParameter(type::UserSchema::key_user_id.data());
+            }
+
+            const auto user_exist_result = service::UserServices::id_exist(user_id);
+            if (not user_exist_result.has_value()) {
+                auto response = type::BasicResponse{.code    = k500InternalServerError,
+                                                    .message = "UserIdExistMiddleware k500InternalServerError",
+                                                    .result  = "Failed to find user",
+                                                    .data    = ""};
+                mcb(toResponse(response.to_json()));
+                return;
+            }
+
+            if (user_exist_result.value() == false) {
+                auto response = type::BasicResponse{.code    = k404NotFound,
+                                                    .message = "UserIdExistMiddleware k404NotFound",
+                                                    .result  = "User not found",
+                                                    .data    = ""};
+                mcb(toResponse(response.to_json()));
+                return;
+            }
+            nextCb([&, mcb = std::move(mcb)](const HttpResponsePtr& resp) { mcb(resp); });
+        }
+        catch (const std::exception& e) {
+            exception::ExceptionHandler::handle(req, std::move(mcb), e);
+        }
+    }
 
     void UserEmailExistMiddleware::invoke(const HttpRequestPtr&    req,
                                           MiddlewareNextCallback&& nextCb,
                                           MiddlewareCallback&&     mcb) {
-        // todo
-        nextCb([&, mcb = std::move(mcb)](const HttpResponsePtr& resp) { mcb(resp); });
+        service::Logger::info("UserEmailExistMiddleware::invoke");
+
+        try {
+            std::string user_email{};
+            if (req->method() != HttpMethod::Get) {
+                user_email = req->getJsonObject()->operator[](type::UserSchema::key_email.data()).as<std::string>();
+            }
+            else {
+                user_email = req->getParameter(type::UserSchema::key_email.data());
+            }
+
+            const auto user_email_exist = service::UserServices::email_exist(user_email);
+            if (not user_email_exist.has_value()) {
+                auto response = type::BasicResponse{.code    = k500InternalServerError,
+                                                    .message = "UserEmailExistMiddleware k500InternalServerError",
+                                                    .result  = "Failed to find user",
+                                                    .data    = ""};
+                mcb(toResponse(response.to_json()));
+                return;
+            }
+
+            if (user_email_exist.value() == false) {
+                auto response = type::BasicResponse{.code    = k404NotFound,
+                                                    .message = "UserEmailExistMiddleware k404NotFound",
+                                                    .result  = "User not found",
+                                                    .data    = ""};
+                mcb(toResponse(response.to_json()));
+                return;
+            }
+
+            nextCb([&, mcb = std::move(mcb)](const HttpResponsePtr& resp) { mcb(resp); });
+        }
+        catch (const std::exception& e) {
+            exception::ExceptionHandler::handle(req, std::move(mcb), e);
+        }
     }
 
     void UserNameExistMiddleware::invoke(const HttpRequestPtr&    req,
                                          MiddlewareNextCallback&& nextCb,
                                          MiddlewareCallback&&     mcb) {
-        // todo
-        nextCb([&, mcb = std::move(mcb)](const HttpResponsePtr& resp) { mcb(resp); });
+        service::Logger::info("UserNameExistMiddleware::invoke");
+
+        try {
+            std::string user_name{};
+            if (req->method() != HttpMethod::Get) {
+                user_name = req->getJsonObject()->operator[](type::UserSchema::key_name.data()).as<std::string>();
+            }
+            else {
+                user_name = req->getParameter(type::UserSchema::key_name.data());
+            }
+
+            const auto user_name_exist = service::UserServices::name_exist(user_name);
+
+            if (not user_name_exist.has_value()) {
+                auto response = type::BasicResponse{.code    = k500InternalServerError,
+                                                    .message = "UserNameExistMiddleware k500InternalServerError",
+                                                    .result  = "Failed to find user",
+                                                    .data    = ""};
+                mcb(toResponse(response.to_json()));
+                return;
+            }
+
+            if (user_name_exist.value() == false) {
+                auto response = type::BasicResponse{.code    = k404NotFound,
+                                                    .message = "UserNameExistMiddleware k404NotFound",
+                                                    .result  = "User not found",
+                                                    .data    = ""};
+                mcb(toResponse(response.to_json()));
+                return;
+            }
+            nextCb([&, mcb = std::move(mcb)](const HttpResponsePtr& resp) { mcb(resp); });
+        }
+        catch (const std::exception& e) {
+            exception::ExceptionHandler::handle(req, std::move(mcb), e);
+        }
     }
 }  // namespace filter
